f03a28044883850e82030c7d8211b295b7699eae
Leonard Boyce <leonard@siftlogic.com>
2014-09-29 16:39:01 -0400

    Simple reformatting so I don't go insane

--- Ch_6/simple_neuron.erl
+++ Ch_6/simple_neuron.erl
@@ -1,44 +1,52 @@
-%% This source code and work is provided and developed by DXNN Research Group WWW.DXNNResearch.COM
-%%
-%Copyright (C) 2012 by Gene Sher, DXNN Research Group, CorticalComputer@gmail.com
-%All rights reserved.
-%
-%This code is licensed under the version 3 of the GNU General Public License. Please see the LICENSE file that accompanies this project for the terms of use.
+%%% This source code and work is provided and developed by DXNN Research Group WWW.DXNNResearch.COM
+%%%
+%%% Copyright (C) 2012 by Gene Sher, DXNN Research Group, CorticalComputer@gmail.com
+%%% All rights reserved.
+%%%
+%%% This code is licensed under the version 3 of the GNU General Public License.
+%%% Please see the LICENSE file that accompanies this project for the terms of use.
 
 -module(simple_neuron).
 -compile(export_all).
 
+%% The create function spawns a single neuron, where the weights and the bias
+%% are generated randomly to be between -0.5 and 0.5.
 create()->
-	Weights = [random:uniform()-0.5,random:uniform()-0.5,random:uniform()-0.5],
-	register(neuron, spawn(?MODULE,loop,[Weights])).
-%The create function spawns a single neuron, where the weights and the bias are generated randomly to be between -0.5 and 0.5.
+    Weights = [random:uniform() - 0.5, random:uniform() - 0.5, random:uniform() - 0.5],
+    register(neuron, spawn(?MODULE, loop, [Weights])).
 
+%% The spawned neuron process accepts an input vector, prints it and the weight vector
+%% to the screen, calculates the output, and then sends the output to the contacting process.
+%% The output is also a vector of length one.
 loop(Weights) ->
-	receive 
-		{From, Input} ->
-			io:format("****Processing****~n Input:~p~n Using Weights:~p~n",[Input,Weights]),
-			Dot_Product = dot(Input,Weights,0),
-			Output = [math:tanh(Dot_Product)],
-			From ! {result,Output},
-			loop(Weights)
-	end.
-%The spawned neuron process accepts an input vector, prints it and the weight vector to the screen, calculates the output, and then sends the output to the contacting process. The output is also a vector of length one.
+    receive
+        {From, Input} ->
+            io:format("****Processing****~n Input:~p~n Using Weights:~p~n", [Input, Weights]),
+            Dot_Product = dot(Input, Weights, 0),
+            Output = [math:tanh(Dot_Product)],
+            From ! {result, Output},
+            loop(Weights)
+    end.
 
-	dot([I|Input],[W|Weights],Acc) -> 
-		dot(Input,Weights,I*W+Acc);
-	dot([],[Bias],Acc)->
-		Acc + Bias.
-%The dot product function that we use works on the assumption that the bias is incorporated into the weight list as the last value in that list. After calculating the dot product, the input list will empty out while the weight list will still have the single bias value remaining, which we then add to the accumulator.
+%% The dot product function that we use works on the assumption that the bias is incorporated
+%% into the weight list as the last value in that list. After calculating the dot product, the
+%% input list will empty out while the weight list will still have the single bias value
+%% remaining, which we then add to the accumulator.
+dot([I | Input], [W | Weights], Acc) ->
+    dot(Input, Weights, ((I * W) + Acc));
+dot([], [Bias], Acc)->
+    Acc + Bias.
 
+%% We use the sense function to contact the neuron and send it an input vector. The sense
+%% function ensures that the signal we are sending is a vector of length 2.
 sense(Signal)->
-	case is_list(Signal) and (length(Signal) == 2) of
-		true->
-			neuron ! {self(),Signal},
-			receive
-				{result,Output}->
-				io:format("Output: ~p~n",[Output])
-			end;
-		false->
-			io:format("The Signal must be a list of length 2~n")
-	end.
-%We use the sense function to contact the neuron and send it an input vector. The sense function ensures that the signal we are sending is a vector of length 2.
+    case is_list(Signal) and (length(Signal) == 2) of
+        true->
+            neuron ! {self(), Signal},
+            receive
+                {result, Output}->
+                    io:format("Output: ~p~n", [Output])
+            end;
+        false->
+            io:format("The Signal must be a list of length 2~n")
+    end.
